cc6cc34e8a04af2465032bd38a12d367f3746c58
Cole Thorpen <thorpencole@gmail.com>
2023-01-09 21:22:41 -0800

    openai library req/res

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,36 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 const generateImage = async (req, res) => {
-    res.status(200).json({
-        success: true,
-    });
+    try {
+        const response = await openai.createImage({
+            prompt: 'cartoon garfield gobbling up a glizzy',
+            n: 1, // can change to have user pick more returned images, display more
+            size: '512x512'
+        });
+
+        const imageUrl = response.data.data[0].url
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        });
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated.'
+        });
+    }
 };
 
 module.exports = { generateImage };
